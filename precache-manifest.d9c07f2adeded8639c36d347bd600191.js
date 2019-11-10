350f1e9f750008b492ed27fe113aa4fd6869f7d8
Jason Park <parkjs814@gmail.com>
2019-11-10 14:21:03 +0800

    Updates

---
+++ precache-manifest.d9c07f2adeded8639c36d347bd600191.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecc3295a56f47c1f10fcbed4e28a64f",
+    "url": "/peachy-frontend/index.html"
+  },
+  {
+    "revision": "a2acd2796e98cfbd2747",
+    "url": "/peachy-frontend/static/css/main.f4788150.chunk.css"
+  },
+  {
+    "revision": "1fdb5e21188612a451f6",
+    "url": "/peachy-frontend/static/js/2.fe888061.chunk.js"
+  },
+  {
+    "revision": "a2acd2796e98cfbd2747",
+    "url": "/peachy-frontend/static/js/main.727560af.chunk.js"
+  },
+  {
+    "revision": "5e83ce35eee468825d50",
+    "url": "/peachy-frontend/static/js/runtime-main.e409b1c8.js"
+  },
+  {
+    "revision": "40c8cf731384b22a6a47b18feab9a7a0",
+    "url": "/peachy-frontend/static/media/andywarhol.40c8cf73.jpg"
+  },
+  {
+    "revision": "5d2f53e8e9c00a1ccf7a5842af645cef",
+    "url": "/peachy-frontend/static/media/art.5d2f53e8.jpg"
+  },
+  {
+    "revision": "b0138fde5509796547b2dd836b1c4cf9",
+    "url": "/peachy-frontend/static/media/background.b0138fde.jpg"
+  },
+  {
+    "revision": "d90a859c76f930c34d9ba298cf23f14b",
+    "url": "/peachy-frontend/static/media/baking.d90a859c.jpg"
+  },
+  {
+    "revision": "26a88bbeef2f568988a51b15d499acaa",
+    "url": "/peachy-frontend/static/media/basketball.26a88bbe.jpg"
+  },
+  {
+    "revision": "0ba898b07896eb90acc0d54e8c77437e",
+    "url": "/peachy-frontend/static/media/beer.0ba898b0.jpg"
+  },
+  {
+    "revision": "9ea6da8d158b7c3d8cdc1bfca3afa133",
+    "url": "/peachy-frontend/static/media/bukak.9ea6da8d.jpg"
+  },
+  {
+    "revision": "40132805ca2130ac6bea5c845f3d0e4c",
+    "url": "/peachy-frontend/static/media/camping.40132805.jpg"
+  },
+  {
+    "revision": "e759d25e0b37388e1335a364fceb4c08",
+    "url": "/peachy-frontend/static/media/city1.e759d25e.jpg"
+  },
+  {
+    "revision": "d8466de5c809f4446dddac274535d849",
+    "url": "/peachy-frontend/static/media/city2.d8466de5.jpg"
+  },
+  {
+    "revision": "e62fffa8669c3345a0e1fb5c9b27a1ad",
+    "url": "/peachy-frontend/static/media/city3.e62fffa8.jpg"
+  },
+  {
+    "revision": "c6e8e093a90669a7b24ca75a04c3ba87",
+    "url": "/peachy-frontend/static/media/curvy_border.c6e8e093.png"
+  },
+  {
+    "revision": "51abad458c1fb106ec709e95d78ae971",
+    "url": "/peachy-frontend/static/media/farms.51abad45.jpg"
+  },
+  {
+    "revision": "53d97f73c065ea59fd498e839e09edba",
+    "url": "/peachy-frontend/static/media/garosugil.53d97f73.jpg"
+  },
+  {
+    "revision": "2fc3fb251c876410db163c3ac3aa48b0",
+    "url": "/peachy-frontend/static/media/jake.2fc3fb25.jpg"
+  },
+  {
+    "revision": "3aad07a3c23788124f8f862ce5fcbd71",
+    "url": "/peachy-frontend/static/media/jakelee.3aad07a3.jpg"
+  },
+  {
+    "revision": "2350e71dda1ac1a3f768506107df0f55",
+    "url": "/peachy-frontend/static/media/jason.2350e71d.jpg"
+  },
+  {
+    "revision": "b89754aee3367f44dfc981c728d7276a",
+    "url": "/peachy-frontend/static/media/logo.b89754ae.svg"
+  },
+  {
+    "revision": "a78f85016991038edd1cd5ed7b77f1c0",
+    "url": "/peachy-frontend/static/media/meditation.a78f8501.jpg"
+  },
+  {
+    "revision": "8651d5593c977edc4ffaa1e87866a0bb",
+    "url": "/peachy-frontend/static/media/nature.8651d559.jpg"
+  },
+  {
+    "revision": "9ee3a3d66db08c77518ec9a462b60f10",
+    "url": "/peachy-frontend/static/media/pcbang.9ee3a3d6.jpg"
+  },
+  {
+    "revision": "4e101f5c10637584df395aebd82cd885",
+    "url": "/peachy-frontend/static/media/photography.4e101f5c.jpg"
+  },
+  {
+    "revision": "3f81e573d99599547730fcbd6d44090d",
+    "url": "/peachy-frontend/static/media/schoolfood.3f81e573.jpg"
+  },
+  {
+    "revision": "59dad4c0ba26e755b92a7ce6b7f07156",
+    "url": "/peachy-frontend/static/media/seoul.59dad4c0.jpg"
+  },
+  {
+    "revision": "46c8cc1e94afce87b754a910233898f1",
+    "url": "/peachy-frontend/static/media/skateboarding.46c8cc1e.jpg"
+  },
+  {
+    "revision": "a5f78d7f8d24418fcb75ce17868b2dbf",
+    "url": "/peachy-frontend/static/media/travelling.a5f78d7f.jpg"
+  },
+  {
+    "revision": "bc70c2b0e1fb0995113d86dce4904cfa",
+    "url": "/peachy-frontend/static/media/varisa.bc70c2b0.jpg"
+  },
+  {
+    "revision": "9c132d04024fb1801b60dd4a62ff51d4",
+    "url": "/peachy-frontend/static/media/wenghei.9c132d04.jpg"
+  }
+]);
